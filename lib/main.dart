19a318764ea9410df5246fea219957116c146cdb
Romilkumar Patel <patelromil2800@gmail.com>
2021-07-24 16:22:04 -0300

    updated_code

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import '../screens/auth_screen.dart';
 import 'package:flutter/material.dart';
 import '../screens/chat_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,11 +13,28 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Chaat',
+      title: 'Flutter Chat',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.lightBlue,
+        backgroundColor: Colors.pink,
+        accentColor: Colors.deepPurple,
+        accentColorBrightness: Brightness.dark,
+        buttonTheme: ButtonTheme.of(context).copyWith(
+          buttonColor: Colors.pink,
+          textTheme: ButtonTextTheme.primary,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        ),
       ),
-      home: ChatScreen(),
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.onAuthStateChanged,
+        builder: (ctx, userSnapShot) {
+          if (userSnapShot.hasData) {
+            return ChatScreen();
+          }
+          return AuthScreen();
+        },
+      ),
     );
   }
 }
--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,14 +1,75 @@
+import '../widgets/chat/newMessages.dart';
+import '../widgets/chat/messages.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  // @override
+  // void initState() {
+  //   final fbm = FirebaseMessaging();
+  //   fbm.requestNotificationPermissions();
+  //   fbm.configure(onMessage: (msg) {
+  //     print(msg);
+  //     return;
+  //   }, onLaunch: (msg) {
+  //     print(msg);
+  //     return;
+  //   }, onResume: (msg) {
+  //     print(msg);
+  //     return;
+  //   });
+  //   super.initState();
+  // }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (ctx, index) => Container(
-          padding: EdgeInsets.all(8),
-          child: Text('This Works!'),
+      appBar: AppBar(
+        title: Text('Flutter Chat'),
+        actions: [
+          DropdownButton(
+            icon: Icon(
+              Icons.more_vert,
+              color: Theme.of(context).primaryIconTheme.color,
+            ),
+            items: [
+              DropdownMenuItem(
+                child: Container(
+                  child: Row(
+                    children: [
+                      Icon(Icons.exit_to_app),
+                      SizedBox(
+                        width: 8,
+                      ),
+                      Text('Logout')
+                    ],
+                  ),
+                ),
+                value: 'Logout',
+              )
+            ],
+            onChanged: (itemIdentifier) {
+              if (itemIdentifier == 'Logout') {
+                FirebaseAuth.instance.signOut();
+              }
+            },
+          )
+        ],
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Expanded(
+              child: Messages(),
+            ),
+            NewMessage(),
+          ],
         ),
       ),
     );
