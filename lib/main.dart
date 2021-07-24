6f21dd69f2ff9f13d0c49ddb5cffe3b735e27bfc
Romilkumar Patel <patelromil2800@gmail.com>
2021-07-24 16:46:39 -0300

    Splash_screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import '../screens/splash_screen.dart';
 import '../screens/auth_screen.dart';
 import 'package:flutter/material.dart';
 import '../screens/chat_screen.dart';
@@ -29,6 +30,9 @@ class MyApp extends StatelessWidget {
       home: StreamBuilder(
         stream: FirebaseAuth.instance.onAuthStateChanged,
         builder: (ctx, userSnapShot) {
+          if (userSnapShot.connectionState == ConnectionState.waiting) {
+            return SplashScreen();
+          }
           if (userSnapShot.hasData) {
             return ChatScreen();
           }
